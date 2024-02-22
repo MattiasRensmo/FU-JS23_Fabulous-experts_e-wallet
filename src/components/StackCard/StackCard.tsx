89aad2932f534ed49e7ede86b72ddb8929e78740
Mattias Rensmo <mattias@rensmo.se>
2024-02-22 15:51:25 +0100

    Städat Home lite

--- src/components/StackCard/StackCard.tsx
+++ src/components/StackCard/StackCard.tsx
@@ -6,18 +6,8 @@ interface Props {
   onChangeActive: (card: CreditCard) => void
 }
 
-const StackCard: React.FC<Props> = ({ onChangeActive, cards }: Props) => {
+const StackCard = ({ onChangeActive, cards }: Props) => {
   // EN funktion som bestämmer vilket kort som ska va aktivt o gör alla andra kort inaktiva
-
-  /*   const activateCard = (cardId: any) => { // Loop through the cards array and set the active property to true for the clicked card and false for the others
-   const newCards = cards.map((card) => {
-     if (card.id === cardId) {
-       return { …card, active: true }; 
-      } else { return { …card, active: false };
-     } }); // Set the new cards array 
-     setCards(newCards);
-     }; 
- */
 
   if (!cards) return <></>
   if (!cards.length) return <></>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,21 +1,9 @@
 import { useState } from 'react'
 import { Link } from 'react-router-dom'
 import ActiveContainer from '../components/ActiveContainer/ActiveContainer'
-// import { Card } from '../components/Card/Card'
+import '../components/Card/Card'
 import StackCard from '../components/StackCard/StackCard'
 import { useLocalStorage } from '../hooks/useLocalStorage'
-/* import { Card } from '../components/Card/Card' */
-import '../components/Card/Card'
-
-const cardTest: CreditCard = {
-  cardNum: '1234000012340000',
-  holderName: 'Test Testsson',
-  validYear: '24',
-  validMonth: '2',
-  vendor: 'bitcoin',
-  cvc: '000',
-  active: true,
-}
 
 const Home = () => {
   const { setLocalItem, getLocalItem } = useLocalStorage('cards')
@@ -23,34 +11,11 @@ const Home = () => {
   //Initierar cards o försöker hämta data från local storage
   const [cards, setCards] = useState<CreditCard[] | undefined>(getLocalItem())
 
-  const handleSetClick = () => {
-    //sparar antingen ner nya kortet sist i vår array - eller skapar en helt ny array om det inte finns någon sedan innan
-    cards ? setLocalItem([...cards, cardTest]) : setLocalItem([cardTest])
-
-    // Hämtar o visar det vi precis sparade.
-    setCards(() => getLocalItem())
-  }
-
   /* delete button */
   const deleteCard = (): void => {
     const newC = cards?.filter((card) => !card.active)
     setCards(newC)
     setLocalItem(newC)
-
-    // // Find the index of the active card
-    // const activeCardIndex: number = cards.findIndex(
-    //   (card: CreditCard) => card.active
-    // )
-
-    // // If no active card is found, do nothing
-    // if (activeCardIndex === -1) return
-
-    // // Filter out the active card and set the new cards array
-    // const newCards: CreditCard[] = cards.filter(
-    //   (_: CreditCard, index: number) => index !== activeCardIndex
-    // )
-    // setCards(newCards)
-    // setLocalItem(newCards)
   }
 
   const handleChangeActive = (activeCard: CreditCard) => {
@@ -71,11 +36,10 @@ const Home = () => {
     <div className="add-card-container">
       <main>
         <h1 className="title title__e-wallet">E-WALLET</h1>
-        <button onClick={handleSetClick}>
-          Skapa nytt kort i local storage med testdata
-        </button>
-        <button onClick={deleteCard}>delete</button>
         <ActiveContainer cards={cards} />
+        <button className="" onClick={deleteCard}>
+          Delete card
+        </button>
 
         <StackCard cards={cards} onChangeActive={handleChangeActive} />
         <button className="button button__new-card">
